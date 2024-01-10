cb16aff92dfe6f7dc88a8512f34815d7adc00e6a
MSI\silvi <sicilisi@sheridancollege.ca>
2024-01-10 10:36:34 -0600

    Missle now spawns at the turret

--- Assets/Week 1/Scripts/Mover.cs
+++ Assets/Week 1/Scripts/Mover.cs
@@ -5,6 +5,8 @@ using UnityEngine;
 public class Mover : MonoBehaviour
 {
     public float speed = 3f;
+    public GameObject misslePrefab;
+    public Transform spawn;
     // Start is called before the first frame update
     void Start()
     {
@@ -16,5 +18,10 @@ public class Mover : MonoBehaviour
     {
         float direction = Input.GetAxis("Horizontal");
         transform.Translate(direction * speed * Time.deltaTime, 0, 0);
+
+        if(Input.GetKeyDown(KeyCode.Space))
+        {
+            Instantiate(misslePrefab, spawn.position, spawn.rotation);
+        }
     }
 }
